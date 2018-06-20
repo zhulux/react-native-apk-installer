baa5f64e895d6fc2d9e7ba0f1d5604ae90cc4231
FlowerWrong <sysuyangkang@gmail.com>
2018-06-20 18:08:00 +0800

    let it work for android 7 provider

--- android/src/main/java/com/cnull/apkinstaller/ApkInstallerModule.java
+++ android/src/main/java/com/cnull/apkinstaller/ApkInstallerModule.java
@@ -1,5 +1,7 @@
 package com.cnull.apkinstaller;
 
+import android.os.Build;
+import android.support.v4.content.FileProvider;
 import android.widget.Toast;
 
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -11,49 +13,56 @@ import java.util.Map;
 
 import android.content.Intent;
 import android.net.Uri;
+
 import java.io.File;
 
 public class ApkInstallerModule extends ReactContextBaseJavaModule {
-  private ReactApplicationContext _context = null;
+    private ReactApplicationContext _context = null;
 
-  // private static final String DURATION_SHORT_KEY = "SHORT";
-  // private static final String DURATION_LONG_KEY = "LONG";
+    public ApkInstallerModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        _context = reactContext;
+    }
 
-  public ApkInstallerModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    _context = reactContext;
-  }
+    @Override
+    public String getName() {
+        return "ApkInstaller";
+    }
 
-  @Override
-  public String getName() {
-    return "ApkInstaller";
-  }
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>();
+        return constants;
+    }
 
-  @Override
-  public Map<String, Object> getConstants() {
-    final Map<String, Object> constants = new HashMap<>();
-    // constants.put(DURATION_SHORT_KEY, Toast.LENGTH_SHORT);
-    // constants.put(DURATION_LONG_KEY, Toast.LENGTH_LONG);
-    return constants;
-  }
+    @ReactMethod
+    // public void show(String message, int duration) {
+    public void test(String message) {
+        Toast.makeText(getReactApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
 
-  @ReactMethod
-  // public void show(String message, int duration) {
-  public void test(String message) {
-    Toast.makeText(getReactApplicationContext(), message, Toast.LENGTH_LONG).show();
-  }
+    @ReactMethod
+    public void install(String path) {
+        String cmd = "chmod 777 " + path;
+        try {
+            Runtime.getRuntime().exec(cmd);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        File file = new File(path);
 
-  @ReactMethod
-  public void install(String path) {
-      String cmd = "chmod 777 " +path;
-      try {
-          Runtime.getRuntime().exec(cmd);
-      } catch (Exception e) {
-          e.printStackTrace();
-      }
-      Intent intent = new Intent(Intent.ACTION_VIEW);
-      intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-      intent.setDataAndType(Uri.parse("file://" + path), "application/vnd.android.package-archive");
-      _context.startActivity(intent);
-  }
+        // android 7.0
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            Uri contentUri = FileProvider.getUriForFile(_context, "com.starup.fileProvider", file);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            intent.setDataAndType(contentUri, "application/vnd.android.package-archive");
+        } else {
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");
+        }
+
+        _context.startActivity(intent);
+    }
 }
